4608075aaa601cc7345262b6a504946fa3ea897c
Lucia Cavana <luciacavana@yahoo.com.ar>
2022-12-21 22:54:35 -0300

    Comenzamos con la pagina web

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import store 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app =FastAPI()
+
+@app.get('/')
+def get_list():
+    return[
+        1,2,3,4
+    ]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+    <h1>Hola, bienvenido a mi primera pagina</h1>
+    <p>Mi nombre es lucia, espero que esto no muera aca nada mas</p>
+    """
+
 
 def run():
     store.get_categories()
